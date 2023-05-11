1b49dbfee442602f4d65fca6f100ecc25853fceb
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-05-11 19:20:38 +0400

    remove axios

--- src/__tests__/login.ts
+++ src/__tests__/login.ts
@@ -1,16 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { Mongoose } from 'mongoose'
 import { Server } from 'http'
-import MockAdapter from 'axios-mock-adapter'
-import axios from 'axios'
-import request from 'supertest'
 import runApp from '../helpers/runApp'
 import runMongo from '../helpers/mongo'
 import shutdown from 'http-graceful-shutdown'
 
 describe('Login endpoint', () => {
-  const axiosMock = new MockAdapter(axios)
-
   let server: Server
   let mongoServer: MongoMemoryServer
   let mongoose: Mongoose
@@ -33,20 +28,5 @@ describe('Login endpoint', () => {
         err ? reject(err) : resolve()
       })
     })
-  })
-
-  it('should return user for valid /google request', async () => {
-    const testingGoogleMock = {
-      email: 'john@doe.com',
-      name: 'John Doe',
-    }
-    axiosMock
-      .onGet('https://www.googleapis.com/oauth2/v3/userinfo?access_token=test')
-      .reply(200, testingGoogleMock)
-    const response = await request(server)
-      .post('/login/google')
-      .send({ accessToken: 'test' })
-    expect(response.body.name).toBe(testingGoogleMock.name)
-    expect(response.body.email).toBe(testingGoogleMock.email)
   })
 })
