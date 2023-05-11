d2ba198c04681ce097b1d6fd27c9c08201a3b8d4
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-05-11 21:31:56 +0400

    change config

--- src/helpers/configs.ts
+++ src/helpers/configs.ts
@@ -8,14 +8,12 @@ export const negativePrompt =
 
 export const baseConfig = {
   enhance_prompt: true,
-  height: 512,
   key: env.SD_API_TOKEN,
   model_id: 'anything-v4',
   negative_prompt: negativePrompt,
-  num_inference_steps: 30,
   safety_checker: true,
   samples: 1,
-  width: 512,
+  self_attention: true,
 }
 
 export const httpOptions = {
