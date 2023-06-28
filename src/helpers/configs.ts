7f79939c2fcebba913f168e71f6ff3ef78e58652
avr6ude <canuckchan@gmail.com>
2023-06-28 11:56:20 +0300

    update SD model to illuminati-diffusion + prompts

--- src/helpers/configs.ts
+++ src/helpers/configs.ts
@@ -2,14 +2,15 @@ import { Config } from 'unique-names-generator'
 import { animalDictionary, colorDictionary } from './nameDictionary'
 import env from './env'
 
-export const basePrompt = 'fantasy, cute animal, animal, cute'
+export const basePrompt =
+  'fantasy, cute animal, animal, cute, ultra realistic close up portrait, hyper detail, cinematic lighting, magic neon, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K'
 export const negativePrompt =
-  'anime, anime girl, anime woman, anime women, body with camera lens, lens in body, camera in body, watermark, text, signs, furry, furries, abstract, abstract figures, loli, lolicon, shotacon, shota, anime girls with swords'
+  'anime, anime girl, anime woman, anime women, body with camera lens, lens in body, camera in body, watermark, text, signs, furry, furries, abstract, abstract figures, loli, lolicon, shotacon, shota, anime girls with swords, nfixer, nrealfixer, (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry'
 
 export const baseConfig = {
   enhance_prompt: true,
   key: env.SD_API_TOKEN,
-  model_id: 'anything-v4',
+  model_id: 'illuminati-diffusion',
   negative_prompt: negativePrompt,
   safety_checker: true,
   samples: 1,
