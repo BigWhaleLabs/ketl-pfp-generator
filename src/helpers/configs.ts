cb83fb83de977aca5eb293717c479be18e0279c6
avr6ude <canuckchan@gmail.com>
2023-06-28 12:02:41 +0300

    up priority for anime negatives

--- src/helpers/configs.ts
+++ src/helpers/configs.ts
@@ -5,7 +5,7 @@ import env from './env'
 export const basePrompt =
   'fantasy, cute animal, animal, cute, ultra realistic close up portrait, hyper detail, cinematic lighting, magic neon, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K'
 export const negativePrompt =
-  'anime, anime girl, anime woman, anime women, body with camera lens, lens in body, camera in body, watermark, text, signs, furry, furries, abstract, abstract figures, loli, lolicon, shotacon, shota, anime girls with swords, nfixer, nrealfixer, (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry'
+  '(anime, anime girl, anime woman, anime women), body with camera lens, lens in body, camera in body, watermark, text, signs, furry, furries, abstract, abstract figures, loli, lolicon, shotacon, shota, anime girls with swords, nfixer, nrealfixer, (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry'
 
 export const baseConfig = {
   enhance_prompt: true,
