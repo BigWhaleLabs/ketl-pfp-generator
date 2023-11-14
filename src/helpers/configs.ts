84106b0bdca99bddb879d1e498713b6242ee18f1
avr6ude <canuckchan@gmail.com>
2023-11-14 19:21:49 +0100

    Add .DS_Store to the .gitignore file and update the base and anime prompts in the configs file. Also, modify the generatePrompt function in the prompts file to include a random selection between the base and anime prompts. Finally, update the sendImageRequest function to include the style parameter as 'vivid'.

--- src/helpers/configs.ts
+++ src/helpers/configs.ts
@@ -1,17 +1,5 @@
-export const basePrompt =
-  'fantasy, animal, cute, ultra realistic close up portrait, hyper detail, cinematic lighting, magic neon, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K'
-export const negativePrompt =
-  '(anime, anime girl, anime woman, anime women, text), body with camera lens, lens in body, camera in body, watermark, signs, furry, furries, abstract, abstract figures, loli, lolicon, shotacon, shota, anime girls with swords, (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry'
-
-export const baseConfig = {
-  model_id: 'revanimated',
-  negative_prompt: negativePrompt,
-  safety_checker: true,
-  samples: 1,
-  scheduler: 'EulerDiscreteScheduler',
-  self_attention: true,
-  tomesd: false,
-}
+export const basePrompt = `a fantasy profile picture that doesn't include any text with hyper realistic style with neon that's cute, has symmetry and balance with main subject centered in the middle and includes details from user's nickname: `
+export const animePrompt = `an anime waifu picture that doesn't include any text in anime style with neon that's cute, has symmetry and balance with main subject centered in the middle and includes details from user's nickname: `
 
 export const httpOptions = {
   headers: {
